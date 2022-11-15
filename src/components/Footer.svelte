c46ba4ff5d7451aeaaef4c028288ffad570628be
mia-z <iperish.x@gmail.com>
2022-11-15 22:02:13 +0100

    update README with link to repo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <div class={"mt-auto flex flex-row w-full"}>
     <div class={"text-center w-full text-blueGray-400"}>
-        Made with Svelte by <a href={"https://github.com/mia-z"} class={"text-pink-300"}>miaz</a> - Info, Bug Tracker and Source can be found <a class={"text-black dark:text-white"} href={""}>here</a> - YEE wins
+        Made with Svelte by <a href={"https://github.com/mia-z"} class={"text-pink-300"}>miaz</a> - Info, Bug Tracker and Source can be found <a class={"text-black dark:text-white"} href={"https://github.com/mia-z/dgg-exchange-svelte"}>here</a> - YEE wins
     </div>
 </div>
